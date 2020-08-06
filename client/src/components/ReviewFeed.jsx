b6df33829496dab62cfe0e7e9853abd7cbeedf66
Angie davidson <adavidson1@gmail.com>
2020-08-05 22:27:56 -0600

    hard coding features of the tile to the correct spot. Still working on formatting. Created a starRating file copied for material ui - have not refactored for this project yet

--- client/src/components/ReviewFeed.jsx
+++ client/src/components/ReviewFeed.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import ReviewTile from "./ReviewTile.jsx"
 
+
 class ReviewFeed extends React.Component {
   constructor(props) {
     super(props);
